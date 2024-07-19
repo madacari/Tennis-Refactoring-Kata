db48abbec244bb8d6e12fb28feaf086d6ddc050a
M63451 <manuel.camargo@ext.bpifrance.fr>
2024-07-19 14:04:21 +0200

    refactor: extract client method to make dojo more akin to a real codebase

---
+++ typescript-jest/src/MyScoringApplication.ts
@@ -0,0 +1,16 @@
+import { TennisGame } from "./TennisGame";
+
+export function checkScore(player1Score: number, player2Score: number): string {
+    const game = new TennisGame('player1', 'player2')
+
+    const highestScore: number = Math.max(player1Score, player2Score);
+    for (let i = 0; i < highestScore; i++) {
+      if (i < player1Score) {
+        game.wonPoint('player1');
+      }
+      if (i < player2Score) {
+        game.wonPoint('player2');
+      }
+    }
+    return game.getScore();
+  }
--- typescript-jest/tests/TennisTest.spec.ts
+++ typescript-jest/tests/TennisTest.spec.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import {TennisGame} from '../src';
+import { checkScore } from '../src/MyScoringApplication';
 
 function getAllScores(): Array<[number, number, string]> {
   const testCases = path.resolve(__dirname, 'scores.json');
@@ -11,23 +11,10 @@ function getAllScores(): Array<[number, number, string]> {
 
 const scores: Array<[number, number, string]> = getAllScores();
 
-function checkScore(game: TennisGame, player1Score: number, player2Score: number, expectedScore: string): void {
-  const highestScore: number = Math.max(player1Score, player2Score);
-  for (let i = 0; i < highestScore; i++) {
-    if (i < player1Score) {
-      game.wonPoint('player1');
-    }
-    if (i < player2Score) {
-      game.wonPoint('player2');
-    }
-  }
-  expect(game.getScore()).toEqual(expectedScore);
-}
-
 describe('TennisGame', () => {
   scores.forEach(([player1Score, player2Score, expectedScore]) => {
     it(`scores ${player1Score}:${player2Score} as ${expectedScore}`, () => {
-      checkScore(new TennisGame('player1', 'player2'), player1Score, player2Score, expectedScore);
+      expect(checkScore(player1Score, player2Score)).toEqual(expectedScore);
     });
   });
 
